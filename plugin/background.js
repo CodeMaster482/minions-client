f486b8d78c85d3476f7088c56e138ae8cc97bc13
Kosmatoff <dimka.komarov@bk.ru>
2024-10-12 17:04:04 +0300

    типо финальный экземпляр

--- plugin/background.js
+++ plugin/background.js
@@ -1,16 +1,41 @@
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.action === "sendLinks") {
+//     // Отправляем массив ссылок на сервер
+//     console.log(JSON.stringify({ links: message.links }))
+//     fetch("https://127.0.0.1", {
+//       method: "POST",
+//       body: JSON.stringify({ links: message.links })
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//       // Получаем массивы ссылок для зелёных и красных меток
+//       let greenLinks = data.greenLinks || [];
+//       let redLinks = data.redLinks || [];
+      
+//       // Отправляем эти данные обратно в content.js для изменения стилей
+//       chrome.tabs.sendMessage(sender.tab.id, {
+//         action: "highlightLinks",
+//         greenLinks: greenLinks,
+//         redLinks: redLinks
+//       });
+//     })
+//     .catch(error => console.error("Error processing links:", error));
+//   }
+// });
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "sendLinks") {
-    // Отправляем массив ссылок на сервер
-    console.log(JSON.stringify({ links: message.links }))
-    fetch("https://your-backend-server.com/process-links", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ links: message.links })
+    let link = message.links[0];  // Берём только первую ссылку из массива
+   
+    let queryUrl = `http://127.0.0.1:8080/api/scan/url?request=${encodeURIComponent(link)}`; // Корректный синтаксис
+    console.log(queryUrl)
+    // Отправляем запрос с одной ссылкой
+    fetch(queryUrl, {
+      method: "GET"
     })
     .then(response => response.json())
     .then(data => {
+      console.log("response:", data)
       // Получаем массивы ссылок для зелёных и красных меток
       let greenLinks = data.greenLinks || [];
       let redLinks = data.redLinks || [];
@@ -25,3 +50,4 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     .catch(error => console.error("Error processing links:", error));
   }
 });
+
