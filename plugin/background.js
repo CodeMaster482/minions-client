b4f0929b1176f5368a2bf89c2cbc24168eedc070
HomeBase <grigorikovalenko@gmail.com>
2024-10-20 19:51:07 +0300

    [WEB-1][plugin] popup add

--- plugin/background.js
+++ plugin/background.js
@@ -49,3 +49,56 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       return true;
   }
 });
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "processLink") {
+      const { linkDomain } = message;
+
+
+      // Функция для получения цвета для домена
+      async function fetchColor(domain) {
+          const queryUrl = `http://90.156.219.248:8080/api/scan/domain?request=${encodeURIComponent(domain)}`;
+          try {
+              const response = await fetch(queryUrl, {
+                  method: "GET",
+                  headers: {
+                      "Content-Type": "application/json",
+                  }
+              });
+
+              if (!response.ok) {
+                  console.error(`Ошибка при проверке домена ${domain}: ${response.statusText}`);
+                  sendResponse({ color: 'gray' });
+                  return;
+              }
+
+              const data = await response.json();
+
+              // API возвращает { color: "red" }, и т.д.
+              let color;
+              switch (data.color) {
+                  case "Red":
+                      color = 'red';
+                      break;
+                  case "Green":
+                      color = 'green';
+                      break;
+                  case "Gray":
+                  default:
+                      color = 'gray';
+                      break;
+              }
+
+              sendResponse({ color: color });
+          } catch (error) {
+              console.error(`Ошибка при обработке домена ${domain}:`, error);
+              sendResponse({ color: 'gray' });
+          }
+      }
+
+      fetchColor(linkDomain);
+
+      // Возвращаем true, чтобы указать, что ответ будет отправлен асинхронно
+      return true;
+  }
+});
